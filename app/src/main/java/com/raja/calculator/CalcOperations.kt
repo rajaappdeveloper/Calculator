fb3e01a4188653c407ad2c4ee68d38bc38a295c1
rajarajan_115 <rajarajans@fss.co.in>
2024-10-18 15:51:27 +0530

    Initial commit

---
+++ app/src/main/java/com/raja/calculator/CalcOperations.kt
@@ -0,0 +1,17 @@
+package com.raja.calculator
+
+class CalcOperations {
+
+    fun Add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun Subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun Multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+
+}
