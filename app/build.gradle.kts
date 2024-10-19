7e1131e6c47de3ff710a2dfa0bd44f2d9b98cb1a
rajarajan_115 <rajarajans@fss.co.in>
2024-10-19 16:06:28 +0530

    fixes done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-//    alias(libs.plugins.kotlin.library)
+//    alias(libs.plugins.maven.publish)
+    id("maven-publish")
 }
 
 android {
