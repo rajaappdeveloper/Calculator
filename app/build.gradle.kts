0c6cd92bf131521de8b516247b90407daf4f67b1
rajarajan_115 <rajarajans@fss.co.in>
2024-10-19 14:35:46 +0530

    fixes done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
     implementation(project(":CalculatorFunctions"))
 }
