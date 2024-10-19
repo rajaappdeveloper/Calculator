c145512f3e4852c1e337726d26e0ebf4711f1c01
rajarajan_115 <rajarajans@fss.co.in>
2024-10-19 15:53:01 +0530

    fixes done

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+//    alias(libs.plugins.android.tools) apply false
+}
+
+buildscript{
+    dependencies{
+
+        classpath("com.android.tools.build:gradle:8.1.1")
+    }
 }
